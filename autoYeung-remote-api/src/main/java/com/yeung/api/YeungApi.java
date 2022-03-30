1d3bb2e6744fcc3f4d1b9dbc7f293c18181de852
qingqingF <msfengqingqing@gmail.com>
2022-03-30 09:22:31 -0400

    cherry pick test

--- autoYeung-remote-api/src/main/java/com/yeung/api/YeungApi.java
+++ autoYeung-remote-api/src/main/java/com/yeung/api/YeungApi.java
@@ -316,4 +316,12 @@ public class YeungApi extends BaseApi{
         return ResponseData.getInstance(StatusCode.SUCCESS);
     }
 
+
+    public ResponseData testRebase(){
+        int commit1;
+        int commit2;
+        int commit3;
+        return ResponseData.getInstance(StatusCode.SUCCESS);
+    }
+
 }
